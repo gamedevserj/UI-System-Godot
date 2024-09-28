fbdeccdebda47f94fbfcdfa8188ec5017f92795c
serj <13497925+gamedevserj@users.noreply.github.com>
2024-09-28 16:27:03 +0400

    Setup slider view animation

--- UISystem/Common/ElementViews/HSliderView.cs
+++ UISystem/Common/ElementViews/HSliderView.cs
@@ -1,8 +1,105 @@
 ﻿using Godot;
+using UISystem.Common.Enums;
 using UISystem.Common.Interfaces;
+using UISystem.Common.Resources;
 
 namespace UISystem.Common.Elements;
 public partial class HSliderView : HSlider, IFocusableControl
 {
+
+    [Export] private ColorTweenSettings hoverSettings;
+    [Export] private Control sliderBackground;
+    [Export] private Control sliderFill;
+    [Export] private Control grabber;
+
+    private ITweener _tweener;
+    private bool _mouseOver;
+    private bool _isDragging;
+
+    public override async void _EnterTree()
+    {
+        if (hoverSettings == null) return;
+
+        await ToSignal(RenderingServer.Singleton, RenderingServerInstance.SignalName.FramePostDraw);
+
+        _tweener = hoverSettings.CreateTweener(GetTree(), grabber);
+        Subscribe();
+        UpdateSliderVisual();
+    }
+
+    public override void _ExitTree() => Unsubscribe();
+
+    public override void _Process(double delta)
+    {
+        if (hoverSettings == null) return;
+        if (_isDragging)
+        {
+            UpdateSliderVisual();
+        }
+    }
+
+    private void Subscribe()
+    {
+        FocusEntered += OnFocusEntered;
+        FocusExited += OnFocusExited;
+        MouseEntered += OnMouseEntered;
+        MouseExited += OnMouseExited;
+        DragStarted += OnDragStarted;
+        DragEnded += OnDragEnded;
+    }
+
+    private void Unsubscribe()
+    {
+        FocusEntered -= OnFocusEntered;
+        FocusExited -= OnFocusExited;
+        MouseEntered -= OnMouseEntered;
+        MouseExited -= OnMouseExited;
+        DragStarted -= OnDragStarted;
+        DragEnded -= OnDragEnded;
+    }
+
+    private void OnMouseEntered()
+    {
+        _mouseOver = true;
+        _tweener.OnMouseEntered(GetDrawingMode());
+    }
+    private void OnMouseExited()
+    {
+        _mouseOver = false;
+        _tweener.OnMouseExited(GetDrawingMode());
+    }
+
+    private void OnFocusEntered()
+    {
+        _tweener.OnFocusEntered(GetDrawingMode());
+    }
+    private void OnFocusExited()
+    {
+        _tweener.OnFocusExited(GetDrawingMode());
+    }
+
+    private ControlDrawMode GetDrawingMode()
+    {
+        if (HasFocus())
+        {
+            return _mouseOver ? ControlDrawMode.HoverFocus : _isDragging ? ControlDrawMode.HoverFocus : ControlDrawMode.Focus;
+        }
+        else
+            return _mouseOver ? ControlDrawMode.Hover : ControlDrawMode.Normal;
+    }
+
+    private void OnDragStarted() => _isDragging = true;
+
+    private void OnDragEnded(bool changed)
+    {
+        _isDragging = false;
+        _tweener.OnMouseExited(GetDrawingMode());
+    }
+
+    private void UpdateSliderVisual()
+    {
+        grabber.Position = new Vector2((sliderBackground.Size.X * (float)Value) - grabber.Size.X * 0.5f, grabber.Position.Y);
+        sliderFill.Size = new Vector2((sliderBackground.Size.X * (float)Value), sliderFill.Size.Y);
+    }
 
 }
