d070013321bf08ace74ea4d7fd2985a4e073072d
serj <13497925+gamedevserj@users.noreply.github.com>
2025-09-01 20:19:45 +0400

    Revert "Simplified MenuController"
    This reverts commit a9737e98a85176bb5928a824b593496b91848797.

--- UISystem/MenuSystem/MenuControllerBase.cs
+++ UISystem/MenuSystem/MenuControllerBase.cs
@@ -1,4 +1,5 @@
-﻿using UISystem.Core.MenuSystem;
+﻿using System;
+using UISystem.Core.MenuSystem;
 using UISystem.Core.Views;
 using UISystem.Elements;
 
@@ -6,7 +7,7 @@ namespace UISystem.MenuSystem;
 // just a base class to adapt generic controller to Godot's specific parameters
 // so that there is no need to specify IMenuModel, InputEvent, IFocusableControl for every controller
 internal abstract class MenuControllerBase<TViewCreator, TView>
-    : MenuController<TViewCreator, TView, IFocusableControl>
+    : MenuController<TViewCreator, TView, IMenuModel, IFocusableControl>
     where TViewCreator : IViewCreator<TView>
     where TView : IMenuView<IFocusableControl>
 {
